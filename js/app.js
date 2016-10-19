4df8ecf682c37761a2213b0657ebf95e6830f79b
Clayton <csalem@lastcallmedia.com>
2016-10-19 15:30:24 -0400

    remove overlay on menu item click, add animations

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ $(document).foundation();
 
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
+    // Uncheck the overlay checkbox.
+    $('#hamburger').prop("checked", false);
+
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
